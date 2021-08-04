15cc3ea99ff2441582593c4eb52d55763885fd2a
vikash212000yadav <vikash212000yadav@gmail.com>
2021-08-04 10:15:17 +0530

    use django==1.11.29

--- musica/music/models.py
+++ musica/music/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Album(models.Model):
-    user = models.ForeignKey(User, default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
